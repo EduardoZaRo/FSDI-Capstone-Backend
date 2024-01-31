d8a43b77bf4cc7c8fb73e2f88d4d54a8090c0a59
EduardoZaRo <irvin.zavala@uabc.ed.mx>
2024-01-31 03:53:50 +0000

    Saving

--- ESP32_Django_test/settings.py
+++ ESP32_Django_test/settings.py
@@ -159,21 +159,34 @@ AUTH_USER_MODEL = "pages.User"
 # CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000", "http://0.0.0.0:3000", "http://172.22.205.83:8000", "https://localhost:3000"]
 
 CSRF_TRUSTED_ORIGINS = [
+    # 'https://eduardoZaRo.pythonanywhere.com',
+    # 'ESP32HTTPClient',
     'eduardoZaRo.pythonanywhere.com',
     'https://localhost:3000',
 ]
 CORS_ORIGIN_WHITELIST = [
+    # 'https://eduardoZaRo.pythonanywhere.com',
+    # 'ESP32HTTPClient',
     'eduardoZaRo.pythonanywhere.com',
     'https://localhost:3000',
 ]
 ALLOWED_HOSTS = [
-    'eduardozaro.pythonanywhere.com',
-    'https://localhost:3000',
-]
-CORS_ALLOWED_ORIGINS = [
+    # 'https://eduardoZaRo.pythonanywhere.com',
+    # 'ESP32HTTPClient',
     'eduardoZaRo.pythonanywhere.com',
     'https://localhost:3000',
 ]
+CORS_ALLOWED_ORIGINS = [
+    # 'https://eduardoZaRo.pythonanywhere.com',
+    # 'ESP32HTTPClient',
+    'eduardoZaRo.pythonanywhere.com',
+    'https://localhost:3000',
+]
+# ALLOWED_HOSTS = ['*']
+# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_CREDENTIALS = True
+
+
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = False
